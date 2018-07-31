1a4aacaa60ea3f2fe4096edd27d6750e85f872aa
Ash <asha.mohamed.28@gmail.com>
2018-07-30 23:44:18 -0400

    new website template employed

--- README.md
+++
@@ -1,9 +0,0 @@
-# Personal Website
-A personal website to showcase my projects. 
-
-## Technologies Used
-- HTML5
-- CSS3 
-
-## Preview
-Visit my personal website here: https://asham28.github.io/ 
---
+++ js/custom.js
@@ -0,0 +1,13 @@
+
+(function($) {
+    'use strict';
+
+    // Main Navigation
+    $( '.hamburger-menu' ).on( 'click', function() {
+        $(this).toggleClass('close');
+        $('.site-branding').toggleClass('hide');
+        $('.site-navigation').toggleClass('show');
+        $('.site-header').toggleClass('no-shadow');
+    });
+
+})(jQuery);
